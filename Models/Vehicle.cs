7d0fb69bca8c21e4e88e79c6de978f30b4087eca
unknown <abdullayevemil27042006@gmail.com>
2023-12-11 02:05:13 +0400

    Added Models

---
+++ Models/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace Turbo.az.Models;
+public class Vehicle
+{
+    public int Id { get; set; }
+    public double? Price { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public double? EngineVolume { get; set; }
+}
