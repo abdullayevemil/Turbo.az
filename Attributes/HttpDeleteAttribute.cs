1ef1c016446f2101fe10a90d23c7e101d9458e39
Emil <emil@Emils-MacBook-Pro.local>
2023-12-28 08:46:58 +0400

    Create mvc project

--- Attributes/HttpDeleteAttribute.cs
+++
@@ -1,9 +0,0 @@
-using Turbo.az.Attributes.Base;
-
-namespace Turbo.az.Attributes;
-public class HttpDeleteAttribute : HttpAttribute
-{
-    public HttpDeleteAttribute(string routing) : base(HttpMethod.Delete, routing) { }
-
-    public HttpDeleteAttribute() : base(HttpMethod.Delete, null) { }
-}
