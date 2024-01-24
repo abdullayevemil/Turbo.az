f0e9f02125943505adbe7508a3a6337891eb82f5
EmilAbdullayev <abdullayevemil27042006@gmail.com>
2024-01-25 01:18:59 +0400

    Add logging in middleware

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Turbo.az.Middlewares;
 using Turbo.az.Repositories;
 using Turbo.az.Repositories.Base;
+using Turbo.az.Services;
+using Turbo.az.Services.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<LogMiddleware>();
 
 builder.Services.AddSingleton<IVehicleRepository>(provider => 
 {
@@ -33,6 +38,20 @@ builder.Services.AddSingleton<IUserRepository>(provider =>
     return new UserSqlRepository(connectionString);
 });
 
+builder.Services.AddSingleton<ICustomLogger>(provider => 
+{
+    string connectionStringName = "TurboazDb";
+
+    string? connectionString = builder.Configuration.GetConnectionString(connectionStringName);
+
+    if (string.IsNullOrEmpty(connectionString) || string.IsNullOrWhiteSpace(connectionString)) 
+    {
+        throw new Exception($"connection string {connectionStringName} not found in setting.json");
+    }
+
+    return new SqlLogger(connectionString);
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -40,6 +59,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
+app.UseMiddleware<LogMiddleware>();
 
 app.UseHttpsRedirection();
 
